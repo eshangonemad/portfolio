30119081027f6dac7736ba87ea54ad73afc1d422
eshangonemad <83403698+eshangonemad@users.noreply.github.com>
2024-07-14 15:39:07 +0800

    portos 2

--- script.js
+++ script.js
@@ -42,3 +42,26 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+//TIME
+function updateTime() {
+  const element = document.getElementById("Time");
+  const now = new Date();
+
+  const options = {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+      timeZone: 'Asia/Singapore',
+      timeZoneName: 'short'
+  };
+
+  const formatter = new Intl.DateTimeFormat('en-US', options);
+  const formattedTime = formatter.format(now);
+  const formattedTimeWithUTC = formattedTime.replace('SGT', 'SGT UTC+8');
+  
+  element.textContent = formattedTimeWithUTC;
+}
+
+updateTime();
+setInterval(updateTime, 60000); 
